5cb263a0e04965788e8f8117ceeb87d0f564d3be
soyoungna <hos05360@naver.com>
2018-10-11 15:23:40 +0900

    04_control_1

---
+++ 04_control_1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	 int i;
+	 
+	 printf("정수하나를 입력하시오. : ");
+	 scanf("%d", &i);
+	 
+	 if( i>0 )
+	   {
+	   printf("양수입니다.\n");
+	   }
+	   else if( i<0 )  
+       {
+	   printf("음수입니다.\n");
+       }   
+	   else if (i==0){
+	   	printf("0 입니다");
+	   }      
+	return 0;
+}
